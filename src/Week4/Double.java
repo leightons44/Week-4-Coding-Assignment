9d91b82a423111fa0f9fc33262324465859c75a0
Leighton <leightons44@gmail.com>
2024-01-22 12:56:11 -0700

    week 4 project

--- src/Week4/Double.java
+++ src/Week4/Double.java
@@ -1,12 +1,15 @@
 package Week4;
 
 public class Double {
-	public static void main(String[] args) {
+	public static double[] main(String[] args) {
 		double[] numbers = {3.99, 4.99, 5.99};
+		//This establishes the array of double data type
 		for (int i = 0; i < numbers.length; i++) {
+			//This for loop sets i at zero, less than the length of the array and increments the data by 1 every time
 			double averageReturn = 0;
+			//This establishes an integer of double data type and assigns a new variable of averageReturn to it
 			averageReturn =+ numbers[i];
-		
+		//This takes the variable of averageReturn and adds it to the numbers array, and returns the output of the numbers
 		} return numbers;
 		}
 	
